87edec6c3f6d82b397bf83112455df348e4486c5
Jagss24 <btwitsjagannath12@gmail.com>
2025-04-12 16:17:13 +0530

    fixes

--- src/pages/dashboard/components/AddStudent.tsx
+++ src/pages/dashboard/components/AddStudent.tsx
@@ -37,14 +37,18 @@ const AddStudent = ({
           const formData = new FormData(formElement);
           const name = formData.get('name') as string;
           const rollNumber = formData.get('roll_number') as string;
+          const maths = formData.get('maths') as string;
+          const science = formData.get('science') as string;
+          const english = formData.get('english') as string;
+          const attendance = formData.get('attendance') as string;
           dispatch(
             addStudent({
               name,
               roll_number: rollNumber,
               class: '10th',
               section: 'B',
-              marks: { english: 81, maths: 80, science: 90 },
-              attendance: 55,
+              marks: { english: +english, maths: +maths, science: +science },
+              attendance: +attendance,
             })
           );
           handleClose();
@@ -60,6 +64,27 @@ const AddStudent = ({
           label='Roll Number'
           placeholder='Eg: S0101'
         />
+        <section>
+          <p className='text-base text-subHeading font-medium my-2'>Marks:</p>
+          <section className='grid grid-cols-3 gap-4'>
+            <UiTextInputBase
+              name='english'
+              label='English'
+              placeholder='Eg: 80'
+            />
+            <UiTextInputBase name='maths' label='Maths' placeholder='Eg: 80' />
+            <UiTextInputBase
+              name='science'
+              label='Science'
+              placeholder='Eg: 80'
+            />
+          </section>
+          <UiTextInputBase
+            name='attendance'
+            label='Attendance'
+            placeholder='Eg: 75'
+          />
+        </section>
 
         <section className='flex items-center gap-2 mt-2'>
           <UiButton
